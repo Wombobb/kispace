6563a818429888f2442629037aa2dec0461c3d71
mohamed shegow <shegowmo@gmail.com>
2022-11-14 17:42:18 +0900

    add dotenv-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_072808) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_075656) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "user_id", null: false
+    t.bigint "event_space_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_space_id"], name: "index_bookings_on_event_space_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "event_spaces", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.float "price"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_event_spaces_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_072808) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "event_spaces"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "event_spaces", "users"
 end
