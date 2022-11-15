afa91827bb3dbfe0baaa01dfc5a305e1b451844f
mohamed shegow <shegowmo@gmail.com>
2022-11-15 14:24:05 +0900

    functionility added for adding new listing(view, form, controller)

--- app/controllers/event_spaces_controller.rb
+++ app/controllers/event_spaces_controller.rb
@@ -2,4 +2,24 @@ class EventSpacesController < ApplicationController
   def index
     @event_spaces = EventSpace.all
   end
+
+  def new
+    @event_space = EventSpace.new
+  end
+
+  def create
+    @event_space = EventSpace.new(space_params)
+    if @event_space.save
+      redirect_to event_space_path(@event_space)
+    else
+      render status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def space_params
+    params.require(:event_space).permit(:name, :location, :description, :user_id)
+  end
+
 end
