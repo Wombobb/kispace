96903538fd6e64c645086aa1b5ae773a631d69df
thomasprice <thomas.ed.price@gmail.com>
2022-11-15 14:31:40 +0900

    create booking made but ugly

--- app/controllers/event_spaces_controller.rb
+++ app/controllers/event_spaces_controller.rb
@@ -2,6 +2,7 @@ class EventSpacesController < ApplicationController
 
   def show
     @event_space = EventSpace.find(params[:id])
+    @booking = Booking.new
   end
   def index
     @event_spaces = EventSpace.all
