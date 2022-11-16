d49354d2c0fb9183d4a6101b87f0ca79020de428
Tash Cruz <sebastiantashika@gmail.com>
2022-11-16 14:28:57 +0900

    updated booking and space in dashboard

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event_space
   has_many_attached :photos
+  STATUS = ["pending", "rejected", "approved"]
+  enum status: { pending: "pending", rejected: "rejected", approved: "approved" }, _default: :pending
 end
