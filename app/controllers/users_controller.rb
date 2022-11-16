302848544a961e2cb1ff9cf0b8adb7ca12c009fc
Tash Cruz <sebastiantashika@gmail.com>
2022-11-16 10:43:31 +0900

    added policy to User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
   def dashboard
     @user = current_user
     @event_spaces = EventSpace.where(user_id: @user)
+    authorize @user
+    # authorize @event_spaces
   end
 end
